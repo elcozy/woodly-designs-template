b7ce0811575be02abbe41ebbf1f2ea3eb46fa751
ifeakins <elcozy@users.noreply.github.com>
2020-07-24 20:23:00 +0100

    json changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,14 +63,14 @@ function Contact({ data }) {
                 </div>
                 <div className="contact-below mt-4 py-3">
                     <Row>
-                        <Col xs={6} lg={3} className="my-3 my-md-1 a">
+                        <Col xs={5} lg={3} className="my-3 my-md-1 a">
                             <h4 className="text-left">woodly</h4>
                             <p className="my-2 my-md-3">+36702327589</p>
                             <p className="my-2 my-md-3">woodly@gmail.com</p>
                             <p className="my-2 my-md-3">03, 1999 Inker, LA</p>
                         </Col>
-                        <Col xs={6} lg={3} className="my-3 my-md-1 b">
-                            <h5>Company</h5>
+                        <Col xs={7} lg={3} className="my-3 my-md-1 b">
+                            <h5 className="mb-0 mb-md-3">Company</h5>
                             <Row className="mx-0">
                                 <Col xs={5} className="px-0"><a href="#about">About Us</a></Col>
                                 <Col xs={5} className="px-0 pr-md-2"><a href="#contact">Contact</a></Col>
@@ -79,18 +79,19 @@ function Contact({ data }) {
                                 <Col xs={5} className="px-0"><a href="#">Shop</a></Col>
                             </Row>
                         </Col>
-                        <Col xs={3} lg={2} className="my-3 my-md-1 c mt-4 mt-lg-0 socials">
+                        <Col xs={5} lg={2} className="my-3 my-md-1 c mt-4 mt-lg-0 socials">
                             <h5>Social</h5>
-                            <Row className="text-center">
-                                <Col xs={4} className="p-0 mb-3"><a href="#"><img src={"../img/twit.png"}></img></a></Col>
-                                <Col xs={4} className="p-0 mb-3"><a href="#"><img src={"../img/insta.png"}></img></a></Col>
-                                <Col xs={4} className="p-0 mb-3"><a href="#"><img src={"../img/faceb.png"}></img></a></Col>
-                                <Col xs={4} className="p-0 mb-3"><a href="#"><img src={"../img/linkn.png"}></img></a></Col>
-                                <Col xs={4} className="p-0 mb-3"><a href="#"><img src={"../img/gplus.png"}></img></a></Col>
-                            </Row></Col>
-                        <Col xs={9} lg={4} className="my-3 my-md-1 d">
+                            <div className="text-left d-flex flex-wrap">
+                                <Col xs className="p-0 mb-3"><a href="#"><img className="img-fluid" src={"../img/twit.png"}></img></a></Col>
+                                <Col xs className="p-0 mb-3"><a href="#"><img className="img-fluid" src={"../img/insta.png"}></img></a></Col>
+                                <Col xs className="p-0 mb-3"><a href="#"><img className="img-fluid" src={"../img/faceb.png"}></img></a></Col>
+                                <Col xs className="p-0 mb-3"><a href="#"><img className="img-fluid" src={"../img/linkn.png"}></img></a></Col>
+                                <Col xs className="p-0 mb-3"><a href="#"><img className="img-fluid" src={"../img/gplus.png"}></img></a></Col>
+                            </div>
+                        </Col>
+                        <Col xs={7} lg={4} className="my-3 my-md-1 d">
                             <h5>Stay Informed</h5>
-                            <p className="font-weight-normal">Get the scoop on new products and more.</p>
+                            <p className="font-weight-normal mb-2">Get the scoop on new products and more.</p>
                             <Form className="position-relative sign-up ">
                                 <Form.Control placeholder="E-mail" className="border-0" />
                                 <button className="position-absolute m-0 bg-white border-0">Sign Up</button>
@@ -103,11 +104,15 @@ function Contact({ data }) {
             <div className="footer-con py-3 py-md-4">
                 <Container>
                     <div className="d-flex flex-row justify-content-left">
-                        <h5>
-                            woodly. All Rights Reserved 2020 |
+                        <h5 className="pr-md-3">
+                            woodly. All Rights Reserved 2020
                     </h5>
-                        <a href="#"><h5>Terms & Conditions |</h5></a>
-                        <a href="#"><h5>Privacy Policy</h5></a>
+                        <h5 className="pr-md-3">|
+                    </h5>
+                        <a href="#"><h5 className="pr-md-3">Terms & Conditions</h5></a>
+                        <h5 className="pr-md-3">|
+                    </h5>
+                        <a href="#"><h5 className="pr-md-3">Privacy Policy</h5></a>
                     </div>
                 </Container>
 
